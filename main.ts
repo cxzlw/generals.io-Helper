f40b6680e0291cd67459f0cf44e77576118f1903
AstralLing <84491337+AstralLing@users.noreply.github.com>
2022-04-09 02:13:38 +0800

    feat: show who are fighting

--- main.ts
+++ main.ts
@@ -4,7 +4,6 @@
  *
  * GitHub Repo: https://github.com/AstralLing/generals.io-Helper
  */
-export { };
 
 const GeneralsIOColors = ["red", "lightblue", "green", "teal", "orange", "pink", "purple", "maroon", "yellow", "brown", "blue", "purple-blue"];
 
@@ -22,7 +21,7 @@ var isAlive = {
   "blue": false,
   "purple-blue": false
 };
-var lastGame = {
+var lastTurn = {
   "red": 1,
   "lightblue": 1,
   "green": 1,
@@ -34,13 +33,14 @@ var lastGame = {
   "yellow": 1,
   "brown": 1,
   "blue": 1,
-  "purple-blue": 1
+  "purple-blue": 1,
+  "id": 0
 }
 var cities: { x: number, y: number }[] = [];
 var generals: { x: number, y: number, color: string }[] = [];
-var gameObserver: any;
+var mapObserver: MutationObserver, turnObserver: MutationObserver;
 
-function startObserve() {
+function startObserve(): void {
   let observeTarget = document.getElementById("react-container");
   let observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
@@ -50,7 +50,7 @@ function startObserve() {
       });
       mutation.removedNodes.forEach(function (removed: any) {
         if (removed.id === "game-page")
-          gameObserver.disconnect();
+          mapObserver.disconnect();
       });
     });
   });
@@ -58,15 +58,16 @@ function startObserve() {
   observer.observe(observeTarget, { childList: true, subtree: true });
 }
 
-function meow() {
-  let leaderboard = document.getElementById("game-leaderboard");
-  let playerInfo = leaderboard.children[0].children;
-  gameObserver = new MutationObserver(function (mutations) {
+function meow(): void {
+  let turncounter = document.getElementById("turn-counter");
+  let playerInfo = document.getElementById("game-leaderboard").children[0].children;
+  mapObserver = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
       if (mutation.type === "characterData")
         rewriteGame();
     });
   });
+
   for (let color of GeneralsIOColors)
     for (let i = 1; i < playerInfo.length; ++i)
       if (playerInfo[i].children[1].classList.contains(color))
@@ -79,20 +80,20 @@ function meow() {
 
   for (let i = 1, cur: string; i < playerInfo.length; ++i) {
     cur = playerInfo[i].children[1].className.split(' ')[1];
-    lastGame[cur] = 1;
+    lastTurn[cur] = 1;
     playerInfo[i].appendChild(document.createElement('td'));
     playerInfo[i].children[4].textContent = "1";
     playerInfo[i].appendChild(document.createElement('td'));
     playerInfo[i].children[5].textContent = "Loading";
   }
 
-  gameObserver.observe(leaderboard, { attributes: true, characterData: true, subtree: true });
+  mapObserver.observe(turncounter, { attributes: true, characterData: true, subtree: true });
 
   cities = [];
   generals = [];
 }
 
-function rewriteGame() {
+function rewriteGame(): void {
   let deads = document.getElementsByClassName("dead");
 
   for (let dead of deads)
@@ -137,27 +138,44 @@ function rewriteGame() {
     }
   }
 
-  let gameTurns = document.getElementById("turn-counter").textContent;
-  gameTurns = gameTurns.match(/\d+/g)[0];
-  
+  let turncounter = document.getElementById("turn-counter").textContent;
+  let gameTurn = Number(turncounter.match(/\d+/g)[0]);
+
+  if (gameTurn === lastTurn.id) return;
+  lastTurn.id = gameTurn;
+
   let playerInfo = document.getElementById("game-leaderboard").children[0].children;
   for (let i = 1, cur: string; i < playerInfo.length; ++i) {
     cur = playerInfo[i].children[1].className.split(' ')[1];
     let army = Number(playerInfo[i].children[2].textContent);
-    let delta = army - lastGame[cur];
+    let delta = army - lastTurn[cur];
 
-    if (delta != 0) {
-      if (Number(gameTurns) % 25 != 0 && delta > 0 &&
-        delta - Number(playerInfo[i].children[4].textContent) <= 2)
-        playerInfo[i].children[4].textContent = delta.toString();
-      
-      playerInfo[i].children[5].textContent = delta.toString();
-      lastGame[cur] = army;
-    } 
+    if (gameTurn % 25 !== 0 && delta > 0 &&
+      delta - Number(playerInfo[i].children[4].textContent) <= 2) // I guess it's impossible to get more than 2 cities in one turn
+      playerInfo[i].children[4].textContent = delta.toString();
+
+    playerInfo[i].children[5].textContent = delta.toString();
+    lastTurn[cur] = army;
+  }
+
+  for (let i = 1; i < playerInfo.length; ++i) {
+    if (Number(playerInfo[i].children[5].textContent) > 0) {
+      playerInfo[i].children[5].setAttribute("class", "");
+      continue;
+    }
+    let isFighting: boolean = false;
+    for (let j = 1; j < playerInfo.length; ++j)
+      if (i !== j && playerInfo[i].children[5].textContent === playerInfo[j].children[5].textContent) {
+        playerInfo[i].children[5].setAttribute("class", playerInfo[j].children[1].getAttribute("class"));
+        isFighting = true;
+        break;
+      }
+    if (!isFighting)
+      playerInfo[i].children[5].setAttribute("class", "");
   }
 }
 
-function getColor(pos: any) {
+function getColor(pos: any): string {
   for (let color of GeneralsIOColors)
     if (pos.classList.contains(color))
       return color;
