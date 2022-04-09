8b85995d8ca4aa25132b319aaf82844d125eb3e9
AstralLing <84491337+AstralLing@users.noreply.github.com>
2022-04-10 00:58:15 +0800

    fix: 2-space good.

--- js/events.js
+++ js/events.js
@@ -1,14 +1,14 @@
 const simulateMD = new MouseEvent('mousedown', {
-    view: window,
-    bubbles: true,
-    cancelable: true
+  view: window,
+  bubbles: true,
+  cancelable: true
 });
 const simulateMU = new MouseEvent('mouseup', {
-    view: window,
-    bubbles: true,
-    cancelable: true
+  view: window,
+  bubbles: true,
+  cancelable: true
 });
 function simulateClick(clickPos) {
-    clickPos.dispatchEvent(simulateMD);
-    clickPos.dispatchEvent(simulateMU);
+  clickPos.dispatchEvent(simulateMD);
+  clickPos.dispatchEvent(simulateMU);
 }
