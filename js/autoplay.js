7aa5437f0d897719e9a1808470cba1376942116e
AstralLing <84491337+AstralLing@users.noreply.github.com>
2022-04-12 00:29:01 +0800

    upd: plan to implement real bfs, modify something

--- js/autoplay.js
+++ js/autoplay.js
@@ -1,30 +1,38 @@
 function moveNeighbor(from, to) {
-  simulateClick(from);
-  simulateClick(to);
+    simulateClick(from);
+    simulateClick(to);
 }
 function BFS() {
-  let gameMap = document.getElementById("gameMap").children[0];
-  let X = gameMap.children.length;
-  let Y = gameMap.children[0].children.length;
-  function tryMove(from, to) {
-    if (to.classList.length == 1 && to.classList.contains("tiny") ||
-      to.classList.length == 2 && to.classList.contains("tiny") && to.classList.contains("attackable"))
-      moveNeighbor(from, to);
-  }
-  ;
-  for (let x = 0; x < X; ++x) {
-    for (let y = 0; y < Y; ++y) {
-      let pos = gameMap.children[x].children[y];
-      if (pos.classList.contains(myColor) && Number(pos.textContent) > 1) {
-        if (x > 0)
-          tryMove(pos, gameMap.children[x - 1].children[y]);
-        if (x < X - 1)
-          tryMove(pos, gameMap.children[x + 1].children[y]);
-        if (y > 0)
-          tryMove(pos, gameMap.children[x].children[y - 1]);
-        if (y < Y - 1)
-          tryMove(pos, gameMap.children[x].children[y + 1]);
-      }
+    let gameMap = document.getElementById("gameMap").children[0];
+    let X = gameMap.children.length;
+    let Y = gameMap.children[0].children.length;
+    function tryMove(from, to) {
+        if (to.classList.length == 1 && to.classList.contains("tiny") ||
+            to.classList.length == 2 && to.classList.contains("tiny") && to.classList.contains("attackable"))
+            moveNeighbor(from, to);
     }
-  }
+    ;
+    for (let x = 0; x < X; ++x) {
+        for (let y = 0; y < Y; ++y) {
+            let pos = gameMap.children[x].children[y];
+            if (pos.classList.contains(myColor) && Number(pos.textContent) > 1) {
+                if (x > 0) {
+                    tryMove(pos, gameMap.children[x - 1].children[y]);
+                    break;
+                }
+                if (x < X - 1) {
+                    tryMove(pos, gameMap.children[x + 1].children[y]);
+                    break;
+                }
+                if (y > 0) {
+                    tryMove(pos, gameMap.children[x].children[y - 1]);
+                    break;
+                }
+                if (y < Y - 1) {
+                    tryMove(pos, gameMap.children[x].children[y + 1]);
+                    break;
+                }
+            }
+        }
+    }
 }
--- js/main.js
+++ js/main.js
@@ -5,127 +5,127 @@
  */
 const generalsioColors = ["red", "lightblue", "green", "teal", "orange", "pink", "purple", "maroon", "yellow", "brown", "blue", "purple-blue"];
 var isAlive = {
-  "red": false,
-  "lightblue": false,
-  "green": false,
-  "teal": false,
-  "orange": false,
-  "pink": false,
-  "purple": false,
-  "maroon": false,
-  "yellow": false,
-  "brown": false,
-  "blue": false,
-  "purple-blue": false
+    "red": false,
+    "lightblue": false,
+    "green": false,
+    "teal": false,
+    "orange": false,
+    "pink": false,
+    "purple": false,
+    "maroon": false,
+    "yellow": false,
+    "brown": false,
+    "blue": false,
+    "purple-blue": false
 };
 var lastTurn = {
-  "red": 1,
-  "lightblue": 1,
-  "green": 1,
-  "teal": 1,
-  "orange": 1,
-  "pink": 1,
-  "purple": 1,
-  "maroon": 1,
-  "yellow": 1,
-  "brown": 1,
-  "blue": 1,
-  "purple-blue": 1,
-  "id": 0
+    "red": 1,
+    "lightblue": 1,
+    "green": 1,
+    "teal": 1,
+    "orange": 1,
+    "pink": 1,
+    "purple": 1,
+    "maroon": 1,
+    "yellow": 1,
+    "brown": 1,
+    "blue": 1,
+    "purple-blue": 1,
+    "id": 0
 };
 var cities = [];
 var generals = [];
 var turnObserver;
 var myColor;
 function startObserve() {
-  let observeTarget = document.getElementById("react-container");
-  let observer = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-      mutation.addedNodes.forEach(function (added) {
-        if (added.id === "game-page")
-          setTimeout(meow, 100);
-      });
-      mutation.removedNodes.forEach(function (removed) {
-        if (removed.id === "game-page")
-          turnObserver.disconnect();
-      });
+    let observeTarget = document.getElementById("react-container");
+    let observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            mutation.addedNodes.forEach(function (added) {
+                if (added.id === "game-page")
+                    setTimeout(meow, 100);
+            });
+            mutation.removedNodes.forEach(function (removed) {
+                if (removed.id === "game-page")
+                    turnObserver.disconnect();
+            });
+        });
     });
-  });
-  observer.observe(observeTarget, { childList: true, subtree: true });
+    observer.observe(observeTarget, { childList: true, subtree: true });
 }
 function meow() {
-  let turncounter = document.getElementById("turn-counter");
-  let playerInfo = document.getElementById("game-leaderboard").children[0].children;
-  turnObserver = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-      if (mutation.type === "characterData")
-        rewriteGame();
+    let turncounter = document.getElementById("turn-counter");
+    let playerInfo = document.getElementById("game-leaderboard").children[0].children;
+    turnObserver = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            if (mutation.type === "characterData")
+                rewriteGame();
+        });
     });
-  });
-  /* Leaderboard */
-  playerInfo[0].appendChild(document.createElement('td'));
-  playerInfo[0].children[4].textContent = "City";
-  playerInfo[0].appendChild(document.createElement('td'));
-  playerInfo[0].children[5].textContent = "Delta";
-  for (let i = 1, cur, lastPos = -1; i < playerInfo.length; ++i) {
-    if (playerInfo[i].children.length === 3) { // is team name
-      playerInfo[i].children[0].removeAttribute("colspan");
-      playerInfo[i].insertBefore(document.createElement('td'), playerInfo[i].firstChild);
-      playerInfo[i].children[0].textContent = "0";
-      playerInfo[i].appendChild(document.createElement('td'));
-      playerInfo[i].children[4].textContent = "1";
-      playerInfo[i].appendChild(document.createElement('td'));
-      playerInfo[i].children[5].textContent = "Loading";
-      lastPos = i;
-      continue;
+    /* Leaderboard */
+    playerInfo[0].appendChild(document.createElement('td'));
+    playerInfo[0].children[4].textContent = "City";
+    playerInfo[0].appendChild(document.createElement('td'));
+    playerInfo[0].children[5].textContent = "Delta";
+    for (let i = 1, cur, lastPos = -1; i < playerInfo.length; ++i) {
+        if (playerInfo[i].children.length === 3) { // is team name
+            playerInfo[i].children[0].removeAttribute("colspan");
+            playerInfo[i].insertBefore(document.createElement('td'), playerInfo[i].firstChild);
+            playerInfo[i].children[0].textContent = "0";
+            playerInfo[i].appendChild(document.createElement('td'));
+            playerInfo[i].children[4].textContent = "1";
+            playerInfo[i].appendChild(document.createElement('td'));
+            playerInfo[i].children[5].textContent = "Loading";
+            lastPos = i;
+            continue;
+        }
+        cur = playerInfo[i].children[1].className.split(' ')[1];
+        playerInfo[i].appendChild(document.createElement('td'));
+        playerInfo[i].children[4].textContent = "1";
+        playerInfo[i].appendChild(document.createElement('td'));
+        playerInfo[i].children[5].textContent = "Loading";
+        lastTurn[cur] = 1, isAlive[cur] = true;
+        if (lastPos !== -1) {
+            let curStars = Number(playerInfo[i].children[0].textContent.match(/\d+/g)[0]);
+            playerInfo[lastPos].children[0].textContent =
+                (Number(playerInfo[lastPos].children[0].textContent) + curStars).toString();
+        }
     }
-    cur = playerInfo[i].children[1].className.split(' ')[1];
-    playerInfo[i].appendChild(document.createElement('td'));
-    playerInfo[i].children[4].textContent = "1";
-    playerInfo[i].appendChild(document.createElement('td'));
-    playerInfo[i].children[5].textContent = "Loading";
-    lastTurn[cur] = 1, isAlive[cur] = true;
-    if (lastPos !== -1) {
-      let curStars = Number(playerInfo[i].children[0].textContent.match(/\d+/g)[0]);
-      playerInfo[lastPos].children[0].textContent =
-        (Number(playerInfo[lastPos].children[0].textContent) + curStars).toString();
+    for (let i = 1; i < playerInfo.length; ++i) {
+        let colorful = document.createElement("span");
+        colorful.setAttribute("style", "color: goldenrod");
+        colorful.textContent = "★ ";
+        if (playerInfo[i].children[1].classList.contains("team-name"))
+            playerInfo[i].children[0].insertBefore(colorful, playerInfo[i].children[0].firstChild);
     }
-  }
-  for (let i = 1; i < playerInfo.length; ++i) {
-    let colorful = document.createElement("span");
-    colorful.setAttribute("style", "color: goldenrod");
-    colorful.textContent = "★ ";
-    if (playerInfo[i].children[1].classList.contains("team-name"))
-      playerInfo[i].children[0].insertBefore(colorful, playerInfo[i].children[0].firstChild);
-  }
-  /* Get my color */
-  let gameMap = document.getElementById("gameMap").children[0];
-  let X = gameMap.children.length;
-  let Y = gameMap.children[0].children.length;
-  for (let x = 0, flag = false; x < X; ++x) {
-    for (let y = 0; y < Y; ++y) {
-      let pos = gameMap.children[x].children[y];
-      if (pos.classList.contains("selectable"))
-        for (let color of generalsioColors)
-          if (pos.classList.contains(color)) {
-            myColor = color;
-            flag = true;
+    /* Get my color */
+    let gameMap = document.getElementById("gameMap").children[0];
+    let X = gameMap.children.length;
+    let Y = gameMap.children[0].children.length;
+    for (let x = 0, flag = false; x < X; ++x) {
+        for (let y = 0; y < Y; ++y) {
+            let pos = gameMap.children[x].children[y];
+            if (pos.classList.contains("selectable"))
+                for (let color of generalsioColors)
+                    if (pos.classList.contains(color)) {
+                        myColor = color;
+                        flag = true;
+                        break;
+                    }
+            if (flag)
+                break;
+        }
+        if (flag)
             break;
-          }
-      if (flag)
-        break;
     }
-    if (flag)
-      break;
-  }
-  /* Start */
-  document.onkeyup = function (event) {
-    var e = event || window.event || arguments.callee.caller.arguments[0];
-    if (e && e.keyCode == 66) // B
-      BFS();
-  };
-  turnObserver.observe(turncounter, { attributes: true, characterData: true, subtree: true });
-  cities = [];
-  generals = [];
+    /* Start */
+    document.onkeyup = function (event) {
+        var e = event || window.event || arguments.callee.caller.arguments[0];
+        if (e && e.keyCode == 66) // B
+            BFS();
+    };
+    turnObserver.observe(turncounter, { attributes: true, characterData: true, subtree: true });
+    cities = [];
+    generals = [];
 }
 startObserve();
